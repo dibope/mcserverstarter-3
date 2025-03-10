55b12f5b74144c586bb53fa45bdab89b30ab749f
dibope <zmwoxnqp@gmail.com>
2025-03-11 00:55:46 +0530

    Update websitebotex.py

--- .github/workflows/websitebotex.py
+++ .github/workflows/websitebotex.py
@@ -102,7 +102,7 @@ try:
     wait = WebDriverWait(driver, 30)
     # Ensure the button is visible
     #startworld = wait.until(EC.visibility_of_element_located((By.XPATH, "//button[contains(@class, 'btn-primary')]")))
-    startworld = wait.until(EC.presence_of_element_located((By.XPATH, "//button[span[text()='Start World']]")))
+    startworld = driver.find_element(By.CSS_SELECTOR, "button.btn-primary")
     
     # Now ensure it’s clickable
     startworld = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn-primary")))
